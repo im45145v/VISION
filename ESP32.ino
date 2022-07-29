56b6478c25f09c275023ef7e7598f31f6488028f
ashish malla <74368820+im45145v@users.noreply.github.com>
2022-07-29 23:59:19 +0530

    Update ESP32.ino

--- ESP32.ino
+++ ESP32.ino
@@ -1,10 +1,13 @@
+//IMPORTING LIBS
 #include <WTV020SD16P.h>
 #include<WiFi.h>
 #include<HardwareSerial.h>
 #include<TinyGPS++.h>
 #include <BlynkSimpleEsp32.h>
+//STARTING SOME FUNCTIONS OF ESP32
 WiFiServer server(80);
 HardwareSerial SerialGPS(1);
 TinyGPSPlus gps;
+//ANNOUNCING FEW CONSTANTS
 const char* ssid="WIFI_NAME";
 const char* pass="WIFI_PASS";
