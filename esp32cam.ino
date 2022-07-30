afa9e1b4e9b2faf9a15ecd8905af136cc3177d2f
kinshukgoel4 <98045941+kinshukgoel4@users.noreply.github.com>
2022-07-30 23:07:04 +0530

    final code for camera module

--- esp32cam.ino
+++ esp32cam.ino
@@ -16,4 +16,39 @@ Serial.println("CAPTURE FAIL");
 server.send(503, "", "");
  return;
  }
- 
+ void
+handleJpg()
+{
+ if (!esp32cam::Camera.changeResolution(hiRes)) {
+Serial.println("SET-HI-RES FAIL");
+ }
+serveJpg();
+}
+void setup(){
+//main setup
+Serial.begin(115200);
+Serial.println();{
+ using namespace esp32cam;
+ Config cfg;
+cfg.setPins(pins::AiThinker);
+cfg.setResolution(hiRes);
+cfg.setBufferCount(2);
+cfg.setJpeg(80);
+ bool ok = Camera.begin(cfg);
+Serial.println(ok ? "CAMERA IS OK" : "CAMERA FAILED");//camera update
+ }
+WiFi.persistent(false);
+WiFi.mode(WIFI_STA);
+WiFi.begin(WIFI_SSID, WIFI_PASS);
+ while (WiFi.status() != WL_CONNECTED) {
+delay(500);//delay for half sec
+ }
+Serial.print("http://");
+Serial.println(WiFi.localIP());
+Serial.println(" /cam.jpg");
+server.on("/cam.jpg", handleJpg);
+server.begin();
+}
+void loop(){
+server.handleClient();
+}//loop to handle the device
