14f7d996419ca80e06369e35d861394d592356ab
ashish malla <74368820+im45145v@users.noreply.github.com>
2022-07-30 23:14:27 +0530

    Update ESP32.ino

--- ESP32.ino
+++ ESP32.ino
@@ -42,3 +42,72 @@ int cleint(){
  }
  client.stop();
 }}
+int Distance(){
+ digitalWrite(Trig, LOW);
+ delayMicroseconds(2);
+ digitalWrite(Trig, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(Trig, LOW);
+ int timee = pulseIn(Echo, HIGH);
+ int d=timee*0.034/2;
+ //distance and other functions
+ if(d<30)
+ return 1;
+ 
+ else
+ return 0;
+}
+
+void Sound(int a,int b)
+  //sound and voice functions setup
+ if(b==1)
+ wtv.playVoice(a);
+ 
+ else
+ return;
+}
+void LOCATION(){
+ Blynk.run();
+ while (SerialGPS.available() > 0)//gps thing
+ {
+ if (gps.encode(SerialGPS.read()))
+ {
+ if (gps.location.isValid())
+ {
+ latitude = gps.location.lat();
+ lat_str = String(latitude , 6);
+ longitude = gps.location.lng();
+ lng_str = String(longitude , 6);
+ Blynk.virtualWrite(V0, 1, latitude, longitude, "Location");
+   //location function
+ }
+ }
+ }
+}
+void setup()
+{
+  //main setup for project
+ Serial.begin(115200);//data transfer rate
+ delay(4000);//4 secs lag
+ WiFi.begin(ssid,pass);
+ while(WiFi.status()!=WL_CONNECTED)
+ {
+ delay(1000);
+ Serial.println("connecting to wifi..");
+ }
+ Serial.println("connected");
+ Serial.println("IP address: ");
+ Serial.println(WiFi.localIP());
+ server.begin();
+ SerialGPS.begin(9600, SERIAL_8N1, 16, 17);
+ Blynk.begin(auth, ssid, pass);
+ Blynk.virtualWrite(V0, "clr"); 
+ wtv.reset();
+}//wifi or network thing
+void loop()
+{
+ obstacle=cleint();
+ distance=Distance();
+ Sound(obstacle,distance);
+ LOCATION();
+}//looping func
