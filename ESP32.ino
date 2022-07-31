61c8ee078725ec626c0b30d402a38157e20f9d66
ashish malla <74368820+im45145v@users.noreply.github.com>
2022-07-31 10:19:46 +0530

    Update ESP32.ino

--- ESP32.ino
+++ ESP32.ino
@@ -1,48 +1,48 @@
-//IMPORTING LIBS                                             //
-#include <WTV020SD16P.h>                                     //
-#include<WiFi.h>                                             //
-#include<HardwareSerial.h>                                   //
-#include<TinyGPS++.h>                                        //
-#include <BlynkSimpleEsp32.h>                                //
-//STARTING SOME FUNCTIONS OF ESP32                           //
-WiFiServer server(80);                                       //
-HardwareSerial SerialGPS(1);                                 //
-TinyGPSPlus gps;                                             //
-//ANNOUNCING FEW CONSTANTS                                   //
-const char* ssid="WIFI_NAME";                                //
-const char* pass="WIFI_PASS";                                //
-                                                             //
-int cleint(){                                                //
-  //WIFI INITIALIZATION                                      //
- WiFiClient client = server.available();                     //
- if (client){                                                //
- String currentLine = "";                                    //
- while (client.connected()){                                 //
- if (client.available()){                                    //
- char c = client.read();                                     //
- if (c == '\n') {                                            //
- if (currentLine.length() == 0) {                            //
- client.println("HTTP/1.1 200 OK");                          //
- client.println("Content-type:text/html");                   //
- client.println();                                           //
- break;                                                      //
- }                                                           //
- else                                                        //
- currentLine = "";                                           //
- }                                                           //
- else if (c != '\r')                                         //
- currentLine += c;                                           //
- if (currentLine.endsWith("GET /S"))                         //
- return 1;                                                   //
- if (currentLine.endsWith("GET /C"))                         //
- return 2;                                                   //
- if (currentLine.endsWith("GET /D"))                         //
- return 3;                                                   //
- }                                                           //
- }                                                           //
- client.stop();                                              //
-}}                                                           //
-int Distance(){                                              //
+//IMPORTING LIBS                                             //                          .//////                                               
+#include <WTV020SD16P.h>                                     //                        /////,,,,,*                                             
+#include<WiFi.h>                                             //                       /////,,.,,#&&.                                           
+#include<HardwareSerial.h>                                   //                        /..,&&%&,&%&                                            
+#include<TinyGPS++.h>                                        //                         *.,,.,,.,,,                                            
+#include <BlynkSimpleEsp32.h>                                //                           ,,,,,,,,.                                            
+//STARTING SOME FUNCTIONS OF ESP32                           //                          %%%,,,,,                                              
+WiFiServer server(80);                                       //                        %%%%%%%#,.#%%.                                          
+HardwareSerial SerialGPS(1);                                 //                      %%%%%%%%%%%% %%%%                                         
+TinyGPSPlus gps;                                             //                     %%%%%%%%%%%%#  %%%/                                        
+//ANNOUNCING FEW CONSTANTS                                   //                    %%%%%%%%%%%%%%# #%%%                                        
+const char* ssid="WIFI_NAME";                                //                    %%%%%%%%%%%%%%(  %%%                                        
+const char* pass="WIFI_PASS";                                //                    /%%%%%%%%%%%%%/  %%%%                                       
+                                                             //                    %%%%%%%%%%%%%%*  #%%,                                       
+int cleint(){                                                //                    #%%%.,,,..#%%%,  .%%,.                                      
+  //WIFI INITIALIZATION                                      //                    %%%%%,,,,,/%%%.   %%#,,                                     
+ WiFiClient client = server.available();                     //                    %%%%%%,,,,,.%%.   %%%,,.                                    
+ if (client){                                                //                    %%%%%%%,,,,,,%    %%%.,,,.                                  
+ String currentLine = "";                                    //                   %%%%%%%%%.,,,.,.  *%%% .,,,,                                 
+ while (client.connected()){                                 //                   %%%%%%%%%%%,.,,,. %%%%,    ,,,,,.,,                          
+ if (client.available()){                                    //                  .%%%%%%%%%%%%#,,,,,%%%%%                                      
+ char c = client.read();                                     //                  (%%%%%%%%%%%%%%(,,,,%%%%                                      
+ if (c == '\n') {                                            //                  %%%%%%%%%%%%%%%% .,,,,%%                                      
+ if (currentLine.length() == 0) {                            //                  (&&&&&&&%%%%%%%%@&,,,,,%                                      
+ client.println("HTTP/1.1 200 OK");                          //                     /&&&&&&&&&&&&@% .,,,                                       
+ client.println("Content-type:text/html");                   //                      #&&&&&&&&&&&@      ,,                                     
+ client.println();                                           //                       &&&&&&&&&&&@        *,                                   
+ break;                                                      //                        &&&&&&&&&&,          *,                                 
+ }                                                           //                         &&&&&&&&&&            *.                               
+ else                                                        //                        @@&&&&&&&&&              *.                             
+ currentLine = "";                                           //                        @@&&&&&&&&&                *                            
+ }                                                           //                    @@@&&&&&&&&&&&&                  *                          
+ else if (c != '\r')                                         //                 @@@@@@@@@&&&&&&&&                     *                        
+ currentLine += c;                                           //               @@@@@@@@&@ &&&&&&&                        *                      
+ if (currentLine.endsWith("GET /S"))                         //            *@@@@@@@@@    &&&&&&&                          *                    
+ return 1;                                                   //        .@@@&@@@@@@       &&&&&&&                           .*                  
+ if (currentLine.endsWith("GET /C"))                         //     *@@@@@@@@&&@         &&&&&&&                             .*                
+ return 2;                                                   //      *@@@@@@@            &&&&&&&                               ,*              
+ if (currentLine.endsWith("GET /D"))                         //        /@@@@(            &&&&&&%                                 ,*            
+ return 3;                                                   //         *@@@@            #&&&&&&&                                  **          
+ }                                                           //          ,#@@@%          @@@@@@@&                                    **        
+ }                                                           //              .           @@@@@@@@@@%                                   **      
+ client.stop();                                              //                          ,****/%&@@@%/*                                  *&    
+}}                                                           // 
+int Distance(){                                              //                                ASCII ART BY @IM45145V                                        
  digitalWrite(Trig, LOW);                                    //
  delayMicroseconds(2);                                       //
  digitalWrite(Trig, HIGH);                                   //
